3ea403359ced290692e9a6984b94de92db7e4600
anonymous <81408679+researcher114@users.noreply.github.com>
2025-02-09 20:17:49 +0800

    Create evaluator.py

---
+++ evaluator.py
@@ -0,0 +1,339 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+import datetime
+import logging
+import time
+from collections import OrderedDict, abc
+from contextlib import ExitStack, contextmanager
+from typing import List, Union
+import torch
+from torch import nn
+
+import os
+import json
+
+
+from detectron2.utils.comm import get_world_size, is_main_process
+from detectron2.utils.logger import log_every_n_seconds
+
+cLass = (
+    # VOC
+    "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat",
+    "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person",
+    "pottedplant", "sheep", "sofa", "train", "tvmonitor",
+    # COCO-20-40
+    "truck", "traffic light", "fire hydrant", "stop sign", "parking meter",
+    "bench", "elephant", "bear", "zebra", "giraffe",
+    "backpack", "umbrella", "handbag", "tie", "suitcase",
+    "microwave", "oven", "toaster", "sink", "refrigerator",
+    # COCO-40-60
+    "frisbee", "skis", "snowboard", "sports ball", "kite",
+    "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket",
+    "banana", "apple", "sandwich", "orange", "broccoli",
+    "carrot", "hot dog", "pizza", "donut", "cake",
+    # COCO-60-80
+    "bed", "toilet", "laptop", "mouse",
+    "remote", "keyboard", "cell phone", "book", "clock",
+    "vase", "scissors", "teddy bear", "hair drier", "toothbrush",
+    "wine glass", "cup", "fork", "knife", "spoon", "bowl",
+    # Unknown
+    "unknown",  
+)
+
+class DatasetEvaluator:
+    """
+    Base class for a dataset evaluator.
+
+    The function :func:`inference_on_dataset` runs the model over
+    all samples in the dataset, and have a DatasetEvaluator to process the inputs/outputs.
+
+    This class will accumulate information of the inputs/outputs (by :meth:`process`),
+    and produce evaluation results in the end (by :meth:`evaluate`).
+    """
+
+    def reset(self):
+        """
+        Preparation for a new round of evaluation.
+        Should be called before starting a round of evaluation.
+        """
+        pass
+
+    def process(self, inputs, outputs):
+        """
+        Process the pair of inputs and outputs.
+        If they contain batches, the pairs can be consumed one-by-one using `zip`:
+
+        .. code-block:: python
+
+            for input_, output in zip(inputs, outputs):
+                # do evaluation on single input/output pair
+                ...
+
+        Args:
+            inputs (list): the inputs that's used to call the model.
+            outputs (list): the return value of `model(inputs)`
+        """
+        pass
+
+    def evaluate(self):
+        """
+        Evaluate/summarize the performance, after processing all input/output pairs.
+
+        Returns:
+            dict:
+                A new evaluator class can return a dict of arbitrary format
+                as long as the user can process the results.
+                In our train_net.py, we expect the following format:
+
+                * key: the name of the task (e.g., bbox)
+                * value: a dict of {metric name: score}, e.g.: {"AP50": 80}
+        """
+        pass
+
+
+class DatasetEvaluators(DatasetEvaluator):
+    """
+    Wrapper class to combine multiple :class:`DatasetEvaluator` instances.
+
+    This class dispatches every evaluation call to
+    all of its :class:`DatasetEvaluator`.
+    """
+
+    def __init__(self, evaluators):
+        """
+        Args:
+            evaluators (list): the evaluators to combine.
+        """
+        super().__init__()
+        self._evaluators = evaluators
+
+    def reset(self):
+        for evaluator in self._evaluators:
+            evaluator.reset()
+
+    def process(self, inputs, outputs):
+        for evaluator in self._evaluators:
+            evaluator.process(inputs, outputs)
+
+    def evaluate(self):
+        results = OrderedDict()
+        for evaluator in self._evaluators:
+            result = evaluator.evaluate()
+            if is_main_process() and result is not None:
+                for k, v in result.items():
+                    assert (
+                        k not in results
+                    ), "Different evaluators produce results with the same key {}".format(k)
+                    results[k] = v
+        return results
+
+
+def inference_on_dataset(
+    model, data_loader, evaluator: Union[DatasetEvaluator, List[DatasetEvaluator], None]
+):
+    """
+    Run model on the data_loader and evaluate the metrics with evaluator.
+    Also benchmark the inference speed of `model.__call__` accurately.
+    The model will be used in eval mode.
+
+    Args:
+        model (callable): a callable which takes an object from
+            `data_loader` and returns some outputs.
+
+            If it's an nn.Module, it will be temporarily set to `eval` mode.
+            If you wish to evaluate a model in `training` mode instead, you can
+            wrap the given model and override its behavior of `.eval()` and `.train()`.
+        data_loader: an iterable object with a length.
+            The elements it generates will be the inputs to the model.
+        evaluator: the evaluator(s) to run. Use `None` if you only want to benchmark,
+            but don't want to do any evaluation.
+
+    Returns:
+        The return value of `evaluator.evaluate()`
+    """
+    num_devices = get_world_size()
+    logger = logging.getLogger(__name__)
+    logger.info("Start inference on {} batches".format(len(data_loader)))
+
+    total = len(data_loader)  # inference data loader must have a fixed length
+    if evaluator is None:
+        # create a no-op evaluator
+        evaluator = DatasetEvaluators([])
+    if isinstance(evaluator, abc.MutableSequence):
+        evaluator = DatasetEvaluators(evaluator)
+    evaluator.reset()
+
+    num_warmup = min(5, total - 1)
+    start_time = time.perf_counter()
+    total_data_time = 0
+    total_compute_time = 0
+    total_eval_time = 0
+    with ExitStack() as stack:
+        if isinstance(model, nn.Module):
+            stack.enter_context(inference_context(model))
+        stack.enter_context(torch.no_grad())
+
+        start_data_time = time.perf_counter()
+        for idx, inputs in enumerate(data_loader):
+            total_data_time += time.perf_counter() - start_data_time
+            if idx == num_warmup:
+                start_time = time.perf_counter()
+                total_data_time = 0
+                total_compute_time = 0
+                total_eval_time = 0
+
+            start_compute_time = time.perf_counter()
+            outputs = model(inputs)
+            
+            # print(outputs)
+            
+            
+            # 进行伪标签的生成
+            image_ids = [item['image_id'] for item in inputs]
+            image_ids = image_ids[0]
+            heights = [item['height'] for item in inputs]
+            heights = heights[0]
+            widths = [item['width'] for item in inputs]
+            widths = widths[0]
+            result = outputs[0]['instances']
+            # 获取预测框坐标
+            pred_boxes = result.get('pred_boxes').tensor.tolist()
+            # pred_boxes = result.get('pred_boxes').tensor.numpy()
+            # 获取置信度分数
+            scores = result.get('scores').tolist()
+
+            # 获取预测类别
+            pred_classes = result.get('pred_classes').tolist()
+            
+            # #写成DSL .json格式
+            json_data = {
+                "imageName": f"full/{image_ids}.jpg",
+                "targetNum": 0,
+                "rects": [],
+                "tags": [],
+                "scores": [],
+                "masks": []
+            }
+
+            # 循环处理每个预测框
+            for i in range(len(pred_boxes)):
+                if scores[i] > 0.8:
+                    rect = [
+                        pred_boxes[i][0],
+                        pred_boxes[i][1],
+                        pred_boxes[i][2],
+                        pred_boxes[i][3]
+                    ]
+                    json_data["rects"].append(rect)
+                    json_data["tags"].append(cLass[pred_classes[i]])
+                    json_data["scores"].append(scores[i])
+                    json_data["masks"].append([])
+                    json_data["targetNum"] += 1
+                else:
+                    continue
+
+            # 构建保存的 JSON 文件路径
+            json_file_path = os.path.join('/home/***/opendet/opendet2/inference-result/voc_coco/07train+12trainval/', f"{image_ids}.jpg.json")
+            if not os.path.exists(json_file_path):
+                # 如果不存在，创建路径
+                os.makedirs(os.path.dirname(json_file_path), exist_ok=True)
+
+            # 保存 JSON 数据到文件
+            with open(json_file_path, 'w', encoding='utf-8') as json_file:
+                json.dump(json_data, json_file, ensure_ascii=False, indent=4)
+            
+            
+            
+            # # 写成pascal-voc .xml格式的标注
+            # annotation = ET.Element('annotation')
+            # ET.SubElement(annotation, 'folder').text = 'VOC2007'
+            # ET.SubElement(annotation, 'filename').text = str(image_ids)
+            # size = ET.SubElement(annotation, 'size')
+            # ET.SubElement(size, 'width').text = str(widths)
+            # ET.SubElement(size, 'height').text = str(heights)
+            # ET.SubElement(size, 'depth').text = '3'
+            
+            # for i in range(len(pred_boxes)):
+            #     if scores[i] > 0.5:
+            #         object_elem = ET.SubElement(annotation, 'object')
+            #         ET.SubElement(object_elem, 'name').text = cLass[pred_classes[i]]
+            #         ET.SubElement(object_elem, 'pose').text = 'Unspecified'
+            #         ET.SubElement(object_elem, 'truncated').text = '0'
+            #         ET.SubElement(object_elem, 'difficult').text = '0'
+            #         bbox = ET.SubElement(object_elem, 'bndbox')
+            #         ET.SubElement(bbox, 'xmin').text = str(int(pred_boxes[i][0])+1)
+            #         ET.SubElement(bbox, 'ymin').text = str(int(pred_boxes[i][1])+1)
+            #         ET.SubElement(bbox, 'xmax').text = str(int(pred_boxes[i][2]))
+            #         ET.SubElement(bbox, 'ymax').text = str(int(pred_boxes[i][3]))
+            #     else:
+            #         continue
+            # # 保存VOC格式标注文件和图像文件
+            # xml_file = os.path.join('/home/**/DSL/via_to_voc/pseudo_produced_by_opendet_VOCformat/224train_16000iter_r2(224+1828probyr1)/', image_ids + '.xml')
+            # with open(xml_file, 'w', encoding='utf-8') as f:
+            #     f.write(ET.tostring(annotation, encoding='unicode'))
+            
+            if torch.cuda.is_available():
+                torch.cuda.synchronize()
+            total_compute_time += time.perf_counter() - start_compute_time
+
+            start_eval_time = time.perf_counter()
+            evaluator.process(inputs, outputs)
+            total_eval_time += time.perf_counter() - start_eval_time
+
+            iters_after_start = idx + 1 - num_warmup * int(idx >= num_warmup)
+            data_seconds_per_iter = total_data_time / iters_after_start
+            compute_seconds_per_iter = total_compute_time / iters_after_start
+            eval_seconds_per_iter = total_eval_time / iters_after_start
+            total_seconds_per_iter = (time.perf_counter() - start_time) / iters_after_start
+            if idx >= num_warmup * 2 or compute_seconds_per_iter > 5:
+                eta = datetime.timedelta(seconds=int(total_seconds_per_iter * (total - idx - 1)))
+                log_every_n_seconds(
+                    logging.INFO,
+                    (
+                        f"Inference done {idx + 1}/{total}. "
+                        f"Dataloading: {data_seconds_per_iter:.4f} s/iter. "
+                        f"Inference: {compute_seconds_per_iter:.4f} s/iter. "
+                        f"Eval: {eval_seconds_per_iter:.4f} s/iter. "
+                        f"Total: {total_seconds_per_iter:.4f} s/iter. "
+                        f"ETA={eta}"
+                    ),
+                    n=5,
+                )
+            start_data_time = time.perf_counter()
+
+    # Measure the time only for this worker (before the synchronization barrier)
+    total_time = time.perf_counter() - start_time
+    total_time_str = str(datetime.timedelta(seconds=total_time))
+    # NOTE this format is parsed by grep
+    logger.info(
+        "Total inference time: {} ({:.6f} s / iter per device, on {} devices)".format(
+            total_time_str, total_time / (total - num_warmup), num_devices
+        )
+    )
+    total_compute_time_str = str(datetime.timedelta(seconds=int(total_compute_time)))
+    logger.info(
+        "Total inference pure compute time: {} ({:.6f} s / iter per device, on {} devices)".format(
+            total_compute_time_str, total_compute_time / (total - num_warmup), num_devices
+        )
+    )
+
+    results = evaluator.evaluate()
+    # An evaluator may return None when not in main process.
+    # Replace it by an empty dict instead to make it easier for downstream code to handle
+    if results is None:
+        results = {}
+    return results
+
+
+@contextmanager
+def inference_context(model):
+    """
+    A context where the model is temporarily changed to eval mode,
+    and restored to previous mode afterwards.
+
+    Args:
+        model: a torch Module
+    """
+    training_mode = model.training
+    model.eval()
+    yield
+    model.train(training_mode)
